00232004260fb22235981f65d23aa00cb42fea0d
Pascale Schmidt <pascale@pixelwerte.de>
2023-10-04 21:03:07 +0200

    updated clay and material previews

--- src/data/mattGlazes.jsx
+++ src/data/mattGlazes.jsx
@@ -1,37 +1,30 @@
 export default [
   {
-    _id: '1',
-    name: 'glaze',
-    value: 'Keine Glasur',
-    src: '/textures/clay_basic/Clay_Beige_BaseColor.png',
-    alt: 'Keine Glasur',
-  },
-  {
     _id: '2',
     name: 'glaze',
-    value: '9897 Lilaeffekt (B)',
-    src: '/textures/clay_basic/Clay_Beige_BaseColor.png',
-    alt: '9897 Lilaeffekt Botz',
+    value: '9865 Eukalyptus (B)',
+    src: '/previews/Preview_B_Eukalyptus.png',
+    alt: '9865 Eukalyptus Botz',
   },
   {
     _id: '3',
     name: 'glaze',
     value: '9863 Vergissmeinnicht (B)',
-    src: '/textures/clay_basic/Clay_Beige_BaseColor.png',
+    src: '/previews/Preview_B_Vergisssmeinnicht.png',
     alt: '9863 Vergissmeinnicht Botz',
   },
   {
     _id: '4',
     name: 'glaze',
     value: '9867 Patina (B)',
-    src: '/glazes/Glaze_Patina_B-transparent.png',
+    src: '/previews/Preview_B_Patina.png',
     alt: '9867 Patina Botz',
   },
   {
     _id: '4',
     name: 'glaze',
     value: '9864 Rosa Fels (B)',
-    src: '/glazes/Glaze_Rosa_Fels_B-transparent.png',
+    src: '/previews/Preview_B_Rosa_Fels.png',
     alt: '9864 Rosa Fels Botz',
   },
 ]
